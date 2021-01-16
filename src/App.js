a74610b1432e8f3420f23688cb0e2df97fb9d2d7
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 12:16:02 -0300

    feat: routing setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import { AuthProvider } from "./services/Auth";
+import PrivateRoute from "./router/PrivateRoute";
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <span>Team 166</span>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
