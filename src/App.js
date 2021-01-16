aa77955a9dd30e4d9ce81bdecb598c13171fed19
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 20:15:50 -0300

    feat: add new routes to route stack

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Entrepreneur from "./pages/Entrepreneur";
 import Investor from "./pages/Investor";
+import ForgotPassword from "./pages/ForgotPassword";
+import RecoveryCode from "./pages/ForgotPassword/RecoveryCode";
 
 const App = () => {
   return (
     <Router>
-        <div>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/entrepreneur" component={Entrepreneur} />
-          <Route exact path="/investor" component={Investor} />
-        </div>
-      </Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/entrepreneur" component={Entrepreneur} />
+        <Route exact path="/investor" component={Investor} />
+        <Route exact path="/forgot-password" component={ForgotPassword} />
+        <Route exact path="/recovery" component={RecoveryCode} />
+      </div>
+    </Router>
   );
 };
 
