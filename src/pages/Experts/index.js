76681a4efb53efdf5de3c7fa936c05ef3b7bdf13
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 20:13:23 -0300

    feat: experts screen

---
+++ src/pages/Experts/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DesktopLayout from "../../components/DesktopLayout";
+import Container from "../../components/Container";
+
+export const Experts = () => {
+  return (
+    <DesktopLayout>
+      <Container>
+        <h1>Experts</h1>
+      </Container>
+    </DesktopLayout>
+  );
+};
+
+export default Experts;
