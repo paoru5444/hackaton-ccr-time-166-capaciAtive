497bab3e3088e7d7dafb8f341d8fcab8fcaa94ae
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 20:13:03 -0300

    feat: business screen

---
+++ src/pages/Business/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DesktopLayout from "../../components/DesktopLayout";
+import Container from "../../components/Container";
+
+export const Business = () => {
+  return (
+    <DesktopLayout>
+      <Container>
+        <h1>Business</h1>
+      </Container>
+    </DesktopLayout>
+  );
+};
+
+export default Business;
