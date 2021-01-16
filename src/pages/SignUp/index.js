6630f3b14085d60f9f9f764da8bf29abdc04fce1
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 20:15:35 -0300

    feat: add new fields on signup screen

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,34 +1,37 @@
 import React, { useCallback } from "react";
 import { withRouter } from "react-router";
-import app from "../../services/base";
+// import app from "../../services/base";
 import { Form } from "../Login/styles";
 import Container from "../../components/Container";
 import { Spacer } from "../../components/Spacer";
 
 const SignUp = ({ history }) => {
-  const handleSignUp = useCallback(
-    async (event) => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
-      try {
-        await app
-          .auth()
-          .createUserWithEmailAndPassword(email.value, password.value);
-        history.push("/");
-      } catch (error) {
-        alert(error);
-      }
-    },
-    [history]
-  );
+  const sendSignUp = (event) => {
+    try {
+      // const { email, password } = event.target.elements;
+      console.log("event", event);
+    } catch (error) {
+      //
+    }
+  };
 
   return (
     <Container>
       <h1>Cadastre-se</h1>
-      <Form onSubmit={handleSignUp}>
+
+      <Spacer />
+      
+      <Form onSubmit={sendSignUp}>
         <label>
           Email
           <input name="email" type="email" placeholder="Email" />
+        </label>
+
+        <Spacer />
+
+        <label>
+          Senha
+          <input name="password" type="password" placeholder="Password" />
         </label>
 
         <Spacer />
@@ -39,8 +42,36 @@ const SignUp = ({ history }) => {
         </label>
 
         <Spacer />
-        
-        <button type="submit">Sign Up</button>
+
+        <label>
+          Data de nascimento
+          <input name="data_nascimento" type="date" placeholder="13/01/1998" />
+        </label>
+
+        <Spacer />
+
+        <label>
+          CPF
+          <input name="cpf" placeholder="CPF" />
+        </label>
+
+        <Spacer />
+
+        <label>
+          Informações sobre a deficiência
+          <input name="pcd_info" placeholder="Informações" />
+        </label>
+
+        <Spacer />
+
+        <label>
+          SID
+          <input name="sid" placeholder="Exemple SID" />
+        </label>
+
+        <Spacer />
+
+        <button type="submit">Cadastrar</button>
       </Form>
     </Container>
   );
