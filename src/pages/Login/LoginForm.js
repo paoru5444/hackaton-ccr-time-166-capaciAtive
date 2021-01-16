74c8d8c1f15d0c854c3e42aef2b71880b8300020
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 20:15:01 -0300

    fix: fix input values

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -1,30 +1,32 @@
 import React from "react";
-
+import { withRouter } from "react-router";
 import { Form } from "./styles";
 import { Spacer } from "../../components/Spacer";
 
-function LoginForm({ ...props }) {
+function LoginForm({ history, ...props }) {
   return (
     <Form {...props}>
       <label>
         E-mail
         <br />
-        <input name="email" type="email" placeholder="Email" />
+        <input name="email" type="email" placeholder="meu@email.com" />
       </label>
 
       <Spacer />
 
       <label>
-        Senha
+        CPF
         <br />
-        <input name="password" type="password" placeholder="Password" />
+        <input name="password" type="password" placeholder="******" />
       </label>
 
       <Spacer />
+
+      <label onClick={() => history.push('/forgot-password')}>Esqueci minha senha</label>
 
       <button type="submit">Log in</button>
     </Form>
   );
 }
 
-export default LoginForm;
+export default withRouter(LoginForm);
