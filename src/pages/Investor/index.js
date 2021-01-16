8caba8e6ff783cbba55cd09ccb1549498ad55866
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 15:40:18 -0300

    feat: investor component

---
+++ src/pages/Investor/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Container from "../../components/Container";
+
+function Investor() {
+  return <Container>Investidor</Container>;
+}
+
+export default Investor;
