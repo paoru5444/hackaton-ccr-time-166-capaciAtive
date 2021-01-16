6f8838325792aefc97dbca7ce4f9cde6c1cfec88
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 20:15:15 -0300

    feat: school screen

---
+++ src/pages/School/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DesktopLayout from "../../components/DesktopLayout";
+import Container from "../../components/Container";
+
+export const School = () => {
+  return (
+    <DesktopLayout>
+      <Container>
+        <h1>School</h1>
+      </Container>
+    </DesktopLayout>
+  );
+};
+
+export default School;
