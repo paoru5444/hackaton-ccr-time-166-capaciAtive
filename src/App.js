004c9928dd2e181aee71597461b5201e5616413d
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 15:39:51 -0300

    refactor: change route and auth rules

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import { AuthProvider } from "./services/Auth";
-import PrivateRoute from "./router/PrivateRoute";
+import Entrepreneur from "./pages/Entrepreneur";
+import Investor from "./pages/Investor";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Router>
+    <Router>
         <div>
-          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/entrepreneur" component={Entrepreneur} />
+          <Route exact path="/investor" component={Investor} />
         </div>
       </Router>
-    </AuthProvider>
   );
 };
 
