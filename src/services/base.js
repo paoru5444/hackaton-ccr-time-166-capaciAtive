dd33d80ac5daaad1516e18cc294148895352578d
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-16 20:16:11 -0300

    feat: change firebase configs

--- src/services/base.js
+++ src/services/base.js
@@ -1,13 +1,12 @@
 import * as firebase from "firebase/app";
-import "firebase/auth";
 
-const app = firebase.default.initializeApp({
+firebase.default.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
 });
 
-export default app;
+export const database = firebase.default.database;
