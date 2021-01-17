0b39f9eb5a8915336cc26829ee6513f45150371e
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-17 01:35:26 -0300

    refactor: layout and componentDidMount validations

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../../components/Container";
+import Card from "../../components/Card";
 import {Spacer} from "../../components/Spacer";
 import { database } from "../../services/base";
-import { Header, Avatar, UserInfo, Content, PageCard } from './styles';
+import { Header, Avatar, UserInfo, Content, IconsWrapper, HeaderItem } from "./styles";
 import { withRouter } from "react-router";
-
+import Icon, { FontAwesome, Feather } from 'react-web-vector-icons';
 export const Home = ({ history }) => {
+  const [isRegistered, setIsRegistered] = useState(false );
+
   useEffect(() => {
+    checkUserRegistered()
     fetchUser();
   }, []);
+
+  const checkUserRegistered = () => {
+    if (isRegistered) {
+      history.push('/program-register')
+    }
+  }
 
   const cardItems = [
     { title: 'Escola', icon: '', link: '/school'},
@@ -31,28 +41,50 @@ export const Home = ({ history }) => {
   };
   
   return (
-    <Container style={{ justifyContent: 'flex-start', paddingLeft: 140, paddingRight: 140 }}>
+    <Container style={{ justifyContent: 'flex-start' }}>
       <Header>
-        <Avatar
-          src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-        />
+        <HeaderItem>
+          <Avatar
+            src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+          />
 
-        <Spacer />
-        
-        <UserInfo>
-          <h4>Matheus Pereira</h4>
-          <h4>Lorem Ipsum</h4>
-        </UserInfo>
+          <Spacer />
+          
+          <UserInfo>
+            <h4>Matheus Pereira</h4>
+            <h4>Lorem Ipsum</h4>
+          </UserInfo>
+        </HeaderItem>
+
+        <IconsWrapper>
+          <FontAwesome
+            name='bell'
+            color='white'
+            size={30}
+          />
+
+<FontAwesome
+            name='bell'
+            color='white'
+            size={30}
+          />
+
+<FontAwesome
+            name='bell'
+            color='white'
+            size={30}
+          />
+        </IconsWrapper>
       </Header>
 
-      <Spacer size={64} />
+      <Spacer size={128} />
 
       <Content>
         {cardItems.map((item) => (
           <>
-            <PageCard onClick={() => history.push(item.link)}>
+            <Card onClick={() => history.push(item.link)}>
               <span>{item.title}</span>
-            </PageCard>
+            </Card>
 
             <Spacer size={32} />
           </>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,8 +3,22 @@ import styled from "styled-components";
 export const Header = styled.nav`
   display: flex;
   width: 100%;
+  justify-content: space-between;
   align-items: center;
   padding-top: 32px;
+  padding-left: 140px;
+  background: rgb(9, 9, 121);
+  background: linear-gradient(
+    40deg,
+    rgba(9, 9, 121, 1) 0%,
+    rgba(0, 212, 255, 1) 100%
+  );
+`;
+
+export const HeaderItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const Avatar = styled.img`
@@ -12,13 +26,19 @@ export const Avatar = styled.img`
   height: 160px;
   border-radius: 50%;
   object-fit: cover;
+  margin-bottom: -40px;
+  border: solid 2.5px #f2f1f4;
 `;
 
-export const UserInfo = styled.section``;
+export const UserInfo = styled.section`
+  color: #f4f4f4;
+  padding-top: 24px;
+`;
 
 export const Content = styled.section`
   width: 100%;
   display: flex;
+  padding-left: 140px;
 `;
 
 export const PageCard = styled.div`
@@ -36,4 +56,8 @@ export const PageCard = styled.div`
   -webkit-box-shadow: 2px 2px 16px -5px rgba(60, 60, 60, 0.5);
   -moz-box-shadow: 2px 2px 16px -5px rgba(60, 60, 60, 0.5);
   box-shadow: 2px 2px 16px -5px rgba(60, 60, 60, 0.5);
+`;
+
+export const IconsWrapper = styled.div`
+  width: 20%;
 `;
