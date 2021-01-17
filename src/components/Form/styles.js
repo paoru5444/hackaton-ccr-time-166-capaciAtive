efb070334d4cbe8a1d79d7609535bae4c402c30d
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-17 01:35:54 -0300

    style: form button color

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -20,11 +20,21 @@ export const FormComponent = styled.form`
 
   button {
     height: 45px;
-    background-color: #2980b9;
     border: none;
     border-radius: 8px;
     color: #fff;
     font-size: 1rem;
     font-weight: bold;
+
+    background: rgb(9,  9,  121);
+    background: linear-gradient(
+      
+      40deg,
+
+           rgba(9,  9,  121,  1) 0%,
+     
+      rgba(0,  212,  255,  1) 100%
+    
+    );
   }
 `;
